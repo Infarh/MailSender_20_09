e04cb26d3a21377442f87a5003ad94382fb1eef5
Infarh <shmachilin@yandex.ru>
2020-09-22 19:32:51 +0300

    Тестовый код отправки почты в консольном приложении

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace ConsoleTests
 {
@@ -6,6 +8,24 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
+            var to = new MailAddress("shmachilin@gmail.com", "Павел");
+            var from = new MailAddress("shmachilin@yandex.ru", "Павел");
+
+            var message = new MailMessage(from, to);
+            //var msg = new MailAddress("user@server.ru", "qwe@ASD.ru");
+
+            message.Subject = "Заголовок письма от " + DateTime.Now;
+            message.Body = "Текст тестового письма + " + DateTime.Now;
+
+            var client = new SmtpClient("smtp.yandex.ru", 587);
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "user_name",
+                Password = "PassWord!"
+            };
+
+            client.Send(message);
 
             Console.WriteLine("Hello World!");
         }
