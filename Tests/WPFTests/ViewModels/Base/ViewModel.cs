e150cd216f79118b9867e1fd6767a698a3889b27
Infarh <shmachilin@yandex.ru>
2020-09-29 19:41:07 +0300

    Реализацция модели-представления

---
+++ Tests/WPFTests/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WPFTests.ViewModels.Base
+{
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
---
+++ Tests/WPFTests/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using WPFTests.ViewModels.Base;
+
+namespace WPFTests.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Тестовое окно";
+
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if(_Title == value) return;
+                _Title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+    }
+}
