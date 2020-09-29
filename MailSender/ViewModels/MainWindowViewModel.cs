3abdad2bec9194b513e3515a56f121ba8a59482d
Infarh <shmachilin@yandex.ru>
2020-09-29 21:19:13 +0300

    Команда отправи почты

--- MailSender/ViewModels/MainWindowViewModel.cs
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Input;
 using MailSender.Data;
 using MailSender.Infrastructure.Commands;
+using MailSender.lib.Interfaces;
 using MailSender.Models;
 using MailSender.ViewModels.Base;
 
@@ -11,6 +12,9 @@ namespace MailSender.ViewModels
 {
     class MainWindowViewModel : ViewModel
     {
+        private readonly IMailService _MailService;
+
+
         private string _Title = "Тестовое окно";
 
         public string Title
@@ -141,10 +145,44 @@ namespace MailSender.ViewModels
 
         #endregion
 
+        #region Command SendMailCommand - Отправка почты
+
+        /// <summary>Отправка почты</summary>
+        private ICommand _SendMailCommand;
+
+        /// <summary>Отправка почты</summary>
+        public ICommand SendMailCommand => _SendMailCommand
+            ??= new LambdaCommand(OnSendMailCommandExecuted, CanSendMailCommandExecute);
+
+        /// <summary>Проверка возможности выполнения - Отправка почты</summary>
+        private bool CanSendMailCommandExecute(object p)
+        {
+            if (SelectedServer is null) return false;
+            if (SelectedSender is null) return false;
+            if (SelectedRecipient is null) return false;
+            if (SelectedMessage is null) return false;
+            return true;
+        }
+
+        /// <summary>Логика выполнения - Отправка почты</summary>
+        private void OnSendMailCommandExecuted(object p)
+        {
+            var server = SelectedServer;
+            var sender = SelectedSender;
+            var recipient = SelectedRecipient;
+            var message = SelectedMessage;
+
+            var mail_sender = _MailService.GetSender(server.Address, server.Port, server.UseSSL, server.Login, server.Password);
+            mail_sender.Send(sender.Address, recipient.Address, message.Subject, message.Body);
+        }
+
         #endregion
 
-        public MainWindowViewModel()
+        #endregion
+
+        public MainWindowViewModel(IMailService MailService)
         {
+            _MailService = MailService;
             Servers = new ObservableCollection<Server>(TestData.Servers);
             Senders = new ObservableCollection<Sender>(TestData.Senders);
             Recipients = new ObservableCollection<Recipient>(TestData.Recipients);
