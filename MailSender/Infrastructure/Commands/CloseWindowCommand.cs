5d47f547867c62a68f1c31d581743881b67c62f4
Infarh <shmachilin@yandex.ru>
2020-09-29 20:21:36 +0300

    Перенос инфраструктуры

---
+++ MailSender/Infrastructure/Commands/CloseWindowCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Windows;
+using MailSender.Infrastructure.Commands.Base;
+
+namespace MailSender.Infrastructure.Commands
+{
+    class CloseWindowCommand : Command
+    {
+        protected override void Execute(object p)
+        {
+            var window = p as Window;
+
+            if (window is null)
+                window = Application.Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsFocused);
+
+            if (window is null)
+                window = Application.Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsActive);
+
+            window?.Close();
+        }
+    }
+}
---
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MailSender.ViewModels.Base;
+
+namespace MailSender.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Тестовое окно";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
