a33419954cfdfd8cec4246551238a5c1364caf54
Infarh <shmachilin@yandex.ru>
2020-10-06 19:35:08 +0300

    Создание потока

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleTests
 {
@@ -6,10 +7,42 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
+            var main_thread = Thread.CurrentThread;
+            var main_thread_id = main_thread.ManagedThreadId;
 
+            main_thread.Name = "Главный поток!";
+
+            var timer_thread = new Thread(TimerMethod);
+            timer_thread.Name = "Поток часов";
+            timer_thread.IsBackground = true;
+            timer_thread.Start();
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Главный поток {0}", i);
+                Thread.Sleep(10);
+            }
 
             Console.WriteLine("Главный поток работу закончил!");
             Console.ReadLine();
+        }
+
+        private static void TimerMethod()
+        {
+            PrintThreadInfo();
+            while (true)
+            {
+                Console.Title = DateTime.Now.ToString("HH:mm:ss.ffff");
+                Thread.Sleep(100);
+                //Thread.SpinWait(10);
+            }
+        }
+
+        private static void PrintThreadInfo()
+        {
+            var thread = Thread.CurrentThread;
+            Console.WriteLine("id:{0}; name:{1}; priority:{2}",
+                thread.ManagedThreadId, thread.Name, thread.Priority);
         }
     }
 }
