c1995965ccddeee7fbdd92f003f0cb636e334a9b
Infarh <shmachilin@yandex.ru>
2020-10-06 19:45:41 +0300

    Передача информации в поток

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading;
 
 namespace ConsoleTests
@@ -17,14 +18,56 @@ namespace ConsoleTests
             timer_thread.IsBackground = true;
             timer_thread.Start();
 
-            for (var i = 0; i < 10; i++)
-            {
-                Console.WriteLine("Главный поток {0}", i);
-                Thread.Sleep(10);
-            }
+            //var printer_thread = new Thread(PrintMessage)
+            //{
+            //    IsBackground = true,
+            //    Name = "Parameter printer"
+            //};
+            //printer_thread.Start("Hello World!");
+
+            var message = "Hello World~!";
+            var count = 10;
+
+            //new Thread(() => PrintMessage(message, count)) { IsBackground = true }.Start();
+
+            var print_task = new PrintMessageTask(message, count);
+            print_task.Start();
+
+            //for (var i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Главный поток {0}", i);
+            //    Thread.Sleep(10);
+            //}
+
+
 
             Console.WriteLine("Главный поток работу закончил!");
             Console.ReadLine();
+        }
+
+        private static void PrintMessage(object parameter)
+        {
+            PrintThreadInfo();
+            var msg = (string)parameter;
+
+            var thread_id = Thread.CurrentThread.ManagedThreadId;
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine("id:{0}\t{1}", thread_id, msg);
+                Thread.Sleep(10);
+            }
+        }
+
+        private static void PrintMessage(string Message, int Count)
+        {
+            PrintThreadInfo();
+
+            var thread_id = Thread.CurrentThread.ManagedThreadId;
+            for (var i = 0; i < Count; i++)
+            {
+                Console.WriteLine("id:{0}\t{1}", thread_id, Message);
+                Thread.Sleep(10);
+            }
         }
 
         private static void TimerMethod()
@@ -43,6 +86,38 @@ namespace ConsoleTests
             var thread = Thread.CurrentThread;
             Console.WriteLine("id:{0}; name:{1}; priority:{2}",
                 thread.ManagedThreadId, thread.Name, thread.Priority);
+        }
+    }
+
+    class PrintMessageTask
+    {
+        private readonly string _Message;
+        private readonly int _Count;
+        private Thread _Thread;
+
+        public PrintMessageTask(string Message, int Count)
+        {
+            _Message = Message;
+            _Count = Count;
+            _Thread = new Thread(ThreadMethod) { IsBackground = true };
+        }
+
+        public void Start()
+        {
+            if (_Thread?.IsAlive == false)
+                _Thread?.Start();
+        }
+
+        private void ThreadMethod()
+        {
+            var thread_id = Thread.CurrentThread.ManagedThreadId;
+            for (var i = 0; i < _Count; i++)
+            {
+                Console.WriteLine("id:{0}\t{1}", thread_id, _Message);
+                Thread.Sleep(10);
+            }
+
+            _Thread = null;
         }
     }
 }
