8f394a3822703481b027e57a00e82a1b3b7d818e
Infarh <shmachilin@yandex.ru>
2020-10-06 20:48:56 +0300

    ОБращение к элементам пользвоательского инетрфейса из потока Id != 1

--- Tests/WPFTests/MainWindow.xaml.cs
+++ Tests/WPFTests/MainWindow.xaml.cs
@@ -1,7 +1,29 @@
-﻿namespace WPFTests
+﻿using System.Threading;
+using System.Windows;
+
+namespace WPFTests
 {
     public partial class MainWindow
     {
         public MainWindow() => InitializeComponent();
+
+        private void ComputeResultButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            new Thread(() =>
+            {
+                var result = GetResultHard();
+                Application.Current.Dispatcher.Invoke(() => ResultText.Text = result);
+            }){ IsBackground = true }.Start();
+        }
+
+        private string GetResultHard()
+        {
+            for (var i = 0; i < 500; i++)
+            {
+                Thread.Sleep(10);
+            }
+
+            return "Hello World";
+        }
     }
 }
