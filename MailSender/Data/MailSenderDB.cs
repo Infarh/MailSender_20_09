9858283c7b03e7c18a4cf0f70a19528f3abf9a38
Infarh <shmachilin@yandex.ru>
2020-10-13 21:11:46 +0300

    Подготовка контекста базы данных

---
+++ MailSender/Data/MailSenderDB.cs
@@ -0,0 +1,20 @@
+﻿using MailSender.lib.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MailSender.Data
+{
+    class MailSenderDB : DbContext
+    {
+        public DbSet<Recipient> Recipients { get; set; }
+
+        public DbSet<Sender> Senders { get; set; }
+
+        public DbSet<Server> Servers { get; set; }
+
+        public DbSet<Message> Messages { get; set; }
+
+        public DbSet<SchedulerTask> SchedulerTasks { get; set; }
+
+        public MailSenderDB(DbContextOptions<MailSenderDB> opt) : base(opt) { }
+    }
+}
---
+++ MailSender/Data/MailSenderDBContextInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MailSender.Data
+{
+    class MailSenderDBContextInitializer : IDesignTimeDbContextFactory<MailSenderDB>
+    {
+        public MailSenderDB CreateDbContext(string[] args)
+        {
+            const string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MailSender.DB;Integrated Security=True";
+
+            var optionsBuilder = new DbContextOptionsBuilder<MailSenderDB>();
+            optionsBuilder.UseSqlServer(connection_string);
+
+            return new MailSenderDB(optionsBuilder.Options);
+        }
+    }
+}
