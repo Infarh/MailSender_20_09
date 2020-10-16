e54220d1651456b6c3baf972ef4772a7519ca0c1
Infarh <shmachilin@yandex.ru>
2020-10-16 20:21:13 +0300

    Отчёт о статистике

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ConsoleTests.Data;
 using ConsoleTests.Data.Entityes;
+using MailSender.lib.Reports;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,77 +14,14 @@ namespace ConsoleTests
     {
         static async Task Main(string[] args)
         {
-            const string connection_str = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Students.DB;Integrated Security=True";
+            var report = new StatisticReport();
 
-            //var servce_collection = new ServiceCollection();
-            //servce_collection.AddDbContext<StudentsDB>(opt => opt.UseSqlServer(connection_str));
+            report.SendedMailsCount = 1000;
 
-            //var services = servce_collection.BuildServiceProvider();
+            report.CreatePackage("statistic.docx");
 
-            //new DbContextOptionsBuilder().sq
-            //new StudentsDB()
 
-            //using (var db = services.GetRequiredService<StudentsDB>())
-            //{
-
-            //}
-
-            using (var db = new StudentsDB(new DbContextOptionsBuilder<StudentsDB>().UseSqlServer(connection_str).Options))
-            {
-                //await db.Database.EnsureCreatedAsync();
-                await db.Database.MigrateAsync();
-
-                var students_count = await db.Students.CountAsync();
-
-                Console.WriteLine("Число студентов в БД = {0}", students_count);
-            }
-
-            using (var db = new StudentsDB(new DbContextOptionsBuilder<StudentsDB>().UseSqlServer(connection_str).Options))
-            {
-                var k = 0;
-                if (await db.Students.CountAsync() == 0)
-                {
-                    for (var i = 0; i < 10; i++)
-                    {
-                        var group = new Group
-                        {
-                            Name = $"Группа {i}",
-                            Description = $"Описание группы {i}",
-                            Students = new List<Student>()
-                        };
-
-                        for (var j = 0; j < 10; j++)
-                        {
-                            var student = new Student
-                            {
-                                Name = $"Студент {k}",
-                                Surname = $"Surname {k}",
-                                Patronymic = $"Patronymic {k}",
-                            };
-                            k++;
-                            group.Students.Add(student);
-                        }
-
-                        await db.Groups.AddAsync(group);
-                    }
-
-                    await db.SaveChangesAsync();
-                }
-            }
-
-            using (var db = new StudentsDB(new DbContextOptionsBuilder<StudentsDB>().UseSqlServer(connection_str).Options))
-            {
-                var students = await db.Students
-                   .Include(s => s.Group) // JOIN
-                   .Where(s => s.Group.Name == "Группа 5")
-                   .ToArrayAsync();
-
-                foreach (var student in students) 
-                    Console.WriteLine("[{0}] {1} - {2}", student.Id, student.Name, student.Group.Name);
-            }
-
-            Console.WriteLine("Главный поток работу закончил!");
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
